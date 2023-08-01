5cc974e499d3e627feaa927840ae9f3056099c4e
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-01 14:54:48 +0900

    [unrated] Title: 카운트 업, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,16 @@
+function solution(start, end) {
+    // 출력결과가 담길 리스트 선언
+    const answer = [];
+    let num = start;
+    // 시작 숫자와 끝 숫자를 받으면 끝 숫자까지 1씩 증감시킨 숫자들을 출력
+    // 숫자는 0보다 크고 50보다 작음
+    if ((start < 0 && start > 50) && (end < 0 && end > 50)) {
+        return;
+    }
+    for (let i = 0; i < (end - start) + 1; i++) {
+        answer.push(num);
+        num++;
+    }
+    // 리스트 반환
+    return answer;
+}
