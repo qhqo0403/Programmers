d8887d281e849e3cbfc7b86c6871d29139507ec6
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-07 02:13:50 +0900

    Update 두 수의 연산값 비교하기.js

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,11 +1,16 @@
 function solution(a, b) {
-    const aToString = a.toString();
-    const bToString = b.toString();
-    
-    const strValue = aToString + bToString;
+    const strValue = a.toString() + b.toString();
     
     if (Number(strValue) === (2 * a * b)) {
         return Number(strValue);
     }
     return Math.max(Number(strValue), (2 * a * b))
+}
+
+// 다른 풀이
+
+function another(a, b) {
+    let num1 = parseInt(a+""+b+"");
+    let num2 = 2*a*b;
+    return num1 > num2 ? num1 : num2;
 }
