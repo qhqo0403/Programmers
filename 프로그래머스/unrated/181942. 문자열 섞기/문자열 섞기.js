ddaf05ecf8630cd30f496deb99c71f0456d88326
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-07 00:31:21 +0900

    Update 문자열 섞기.js

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,5 +1,5 @@
 function solution(str1, str2) {
-    var answer = '';
+    let answer = '';
     const result = [];
     if (str1.length !== str2.length) {
         return;
@@ -11,4 +11,19 @@ function solution(str1, str2) {
     answer = result.join('');
     
     return answer;
+}
+
+// 다른 풀이
+function another01(str1, str2) {
+    let answer = '';
+    
+    for(let i = 0; i < str1.length; i++) {
+        result += str1[i] + str2[i];
+    }
+    
+    return answer;
+}
+
+function another02(str1, str2) {
+    return [...str1].map((x, idx)=> x + str2[idx]).join("");
 }
