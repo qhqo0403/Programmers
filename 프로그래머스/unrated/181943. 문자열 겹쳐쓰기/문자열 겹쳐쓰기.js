8257f291be3bbee857664064aeee253fb42bb1de
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-07 00:20:37 +0900

    Update 문자열 겹쳐쓰기.js

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -8,6 +8,16 @@ function solution(my_string, overwrite_string, s) {
     }
     answer = word.join('');
     
+    return answer;
+}
+
+// 다른 풀이
+function another01(my_string, overwrite_string, s) {
+    let answer = '';
+
+    answer = my_string.substring(0, s) + overwrite_string + my_string.substring(s + overwrite_string.length);
     
     return answer;
 }
+
+// 단순단순!
