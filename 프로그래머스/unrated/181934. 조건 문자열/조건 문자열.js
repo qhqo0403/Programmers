2f609abf04a2f93cd5ceaeacfa31460cb8a066f7
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-08 13:01:37 +0900

    Update 조건 문자열.js

--- 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -13,3 +13,41 @@ function solution(ineq, eq, n, m) {
     
     return expression ? 1 : 0;
 }
+
+// 다른 풀이
+
+// 객체 이용 방식
+const operations = {
+  '>=': (n, m) => n >= m,
+  '<=': (n, m) => n <= m,
+  '>!': (n, m) => n > m,
+  '<!': (n, m) => n < m,
+};
+
+function another01(ineq, eq, n, m) {
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
+};
+// switch문
+function solution(ineq, eq, n, m) {
+    let check = ineq + eq;
+    let result;
+
+    switch(check) {
+        case ">=":
+            result = n >= m;
+            break;
+        case "<=":
+            result = n <= m;
+            break;
+        case ">!":
+            result = n > m;
+            break;
+        case "<!":
+            result = n < m;
+            break;
+    }
+
+    var answer = result ? 1 : 0;
+    return answer;
+}
