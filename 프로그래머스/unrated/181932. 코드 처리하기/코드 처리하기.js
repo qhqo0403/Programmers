37763230ad1b8d609a5f4e06660f187d85eda990
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-09 18:23:01 +0900

    [unrated] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.js
@@ -1,19 +1,17 @@
 function solution(code) {
     let answer = '';
-    let mode = 0;
-    let method = false;
+    let mode = false;
     // code에 1 이 있을 때마다 mode 가 변경
     // mode 가 0일 때 code의 index가 짝수인 글자들을 반환
     // mode 가 1일 때 code의 index가 홀수인 글자들을 반환
     for (let i = 0; i < code.length; i++) {
         if (code[i] === '1') {
-            method = !method;
+            mode = !mode;
         }
-        method ? mode = 1 : mode = 0;
-        if (mode === 0 && i % 2 === 0 && code[i] !== '1') {
+        if (mode && i % 2 === 1 && code[i] !== '1') {
             answer += code[i];
-        } else if (mode === 1 && i % 2 === 1 && code[i] !== '1') {
-            answer += code[i];
+        } else if (!mode && i % 2 === 0 && code[i] !== '1') {
+            answer += code[i]
         }
     }
     
