13d2323c56490b49ee6ccf37adb68e06f5bcc4f7
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-10 21:30:43 +0900

    Update 주사위 게임 2.js

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -9,3 +9,23 @@ function solution(a, b, c) {
     }
     return answer;
 }
+
+// 다른 풀이
+const another = (a, b, c) => {
+    const set = new Set([a, b, c]);
+    switch ([...set].length) {
+        case 1: return calculate([a, b, c], 3);
+        case 2: return calculate([a, b, c], 2);
+        case 3: return calculate([a, b, c]);
+    }
+};
+
+const calculate = (inc, n=1) => {
+    const [a, b, c] = inc;
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= Math.pow(a, i) + Math.pow(b, i) + Math.pow(c, i)
+    }
+
+    return result;
+};
