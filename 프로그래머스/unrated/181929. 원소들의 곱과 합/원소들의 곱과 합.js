871a04e379419f8f586c907e3f8ce617e54fe758
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-11 14:48:53 +0900

    Update 원소들의 곱과 합.js

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -9,3 +9,8 @@ function solution(num_list) {
     
     return multiply < pow**2 ? 1 : 0;
 }
+
+// 다른 풀이
+function another(num_list) {
+    return Math.pow(num_list.reduce((a,b) => a+b), 2) > num_list.reduce((a,b) => a*b) ? 1 : 0;
+}
