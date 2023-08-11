3917e9a21aa545f10fbf0d3d010048b043656b90
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-11 15:13:48 +0900

    Update 이어 붙인 수.js

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -12,3 +12,25 @@ function solution(num_list) {
   
   return Number(odd.join('')) + Number(even.join(''));
 }
+
+// 다른 풀이
+
+// reduce 메서드
+function another01(num_list) {
+  const { odds, evens } = num_list.reduce(({ odds, evens }, num) => {
+    if (num % 2 === 0) {
+        evens.push(num)
+    } else {
+        odds.push(num)
+    }
+    return { odds, evens }
+  }, { odds: [], evens: [] })
+  return Number(odds.join('')) + Number(evens.join(''))
+}
+
+// filter 메서드
+function another02(num_list) {
+  return parseInt(num_list.filter(a=>a%2!=0).join(""))+parseInt(num_list.filter(a=>a%2==0).join(""));
+}
+
+// Number, parseInt 이외에 +, * 를 앞에 붙이는 방법도 있음! -> +odd.join('') , odd.join('')*1
