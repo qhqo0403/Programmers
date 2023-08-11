24cd3c119909edcf07a24dd6cd3c7ff173c88009
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-11 17:40:13 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,14 +1,12 @@
 function solution(num_list) {
-    const answer = [...num_list];
-    
     const last = num_list.slice(-1);
     const secondLast = num_list.slice(-2, -1)
 
     if (last > secondLast) {
-        answer.push(last - secondLast)
+        num_list.push(last - secondLast)
     } else {
-        answer.push(last * 2)
+        num_list.push(last * 2)
     }
     
-    return answer;
+    return num_list;
 }
