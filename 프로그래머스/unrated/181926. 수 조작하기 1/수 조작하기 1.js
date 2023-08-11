ea595c30546096426f319128d571582dd3c34348
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-11 17:50:58 +0900

    [unrated] Title: 수 조작하기 1, Time: 3.49 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,19 @@
+function solution(n, control) {
+    for (let letter of control) {
+        switch(letter) {
+            case "w":
+                n++;
+                break;
+            case "s":
+                n--;
+                break;
+            case "d":
+                n += 10;
+                break;
+            case "a":
+                n -= 10;
+                break;
+        }
+    }
+    return n;
+}
