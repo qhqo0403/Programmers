901d2a2ba3f8b83813236b9dcc37e9d16c4809f8
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-15 00:38:10 +0900

    Update 배열 만들기 4.js

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -4,13 +4,16 @@ function solution(arr) {
     let i = 0;
     
     while(i < arr.length) {
+        // stk.length === 0 이면 arr[i]에 1을 더해서 추가, i + 1
         if (stk.length === 0) {
             stk.push(arr[i]);
             i++;
         }
+        // stk의 마지막 원소가 arr[i]보다 작으면 arr[i]를 마지막에 추가, i + 1
         if (stk[stk.length - 1] < arr[i]) {
             stk.push(arr[i]);
             i++;
+        // stk의 마지막 원소가 arr[i]보다 크면 stk에서 제거
         } else {
             stk.pop();
         }
