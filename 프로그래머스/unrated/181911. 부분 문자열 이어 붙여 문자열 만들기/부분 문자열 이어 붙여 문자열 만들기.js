dd266cbdbee27dccd680aba9b4af93f1ed0b2530
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-15 14:59:16 +0900

    Update 부분 문자열 이어 붙여 문자열 만들기.js

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -9,3 +9,10 @@ function solution(my_strings, parts) {
     
     return answer;
 }
+
+// 다른풀이
+function another(my_strings, parts) {
+    return parts.map(([s, e], i) => {
+        return my_strings[i].slice(s, e + 1)
+    }).join('')
+}
