351d3b3ed811749648d3714a3704cdc7ac244603
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-18 19:00:35 +0900

    Update 배열 조각하기.js

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -10,3 +10,21 @@ function solution(arr, query) {
     
     return arr;
 }
+
+// 다른 풀이
+function another(arr, query) {
+    let start = 0;
+    let end = 0;
+
+    for(let i = 0; i < query.length; i++){
+        if(i%2===0){
+            end = start + query[i];
+        } else {
+            start += query[i];
+        }
+    }
+    return arr.slice(start, end).length ?
+        arr.slice(start, end) : [-1]
+}
+
+// 2개의 포인터
