7907231bdc8726cee2a8be040d6fac30e3f7d835
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-21 23:31:23 +0900

    Update 문자열 나누기.js

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -2,6 +2,35 @@ function solution(s) {
   // 문자열의 첫글자와 같은 글자, 다른 글자를 각각 카운트하는 변수
   // 두 변수의 값이 같아지는 부분만큼의 문자열을 반환
   // 문자열 s의 길이만큼 반복 -> 읽을 글자가 없거나 문자열이 끝나면 종료
+let answer = 0;
+
+let start = 0;
+let same = 0;
+let diff = 0;
+
+for (let i = 0; i < s.length; i++) {
+  if (s[start] === s[i]) {
+    same++;
+  } else {
+    diff++;
+  }
+
+  if (same === diff) {
+    answer++;
+    start = i + 1;
+    same = 0;
+    diff = 0;
+  } else if (same !== diff && i === s.length -1) {
+    answer++;
+  }
+}
+
+return answer;
+}
+// 최대 3.84ms 소요!
+// 위의 방식과 다르게 문제에서 요구한대로 문자열을 분해하고 갯수를 구하는 방식을 반영하고 재귀함수로 만들어보고 싶어서 시간 비교할겸 
+
+function another(s) {
   let answer = [];
 
   function helper(str) {
@@ -25,5 +54,6 @@ function solution(s) {
 
   helper(s);
 
-  return answer.length;
+  return answer;
 }
+// 최대 2.71ms 소요!
