95c4933fdb1baa91352d8f374cdb5f0907d69669
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-21 23:29:23 +0900

    [unrated] Title: 문자열 나누기, Time: 2.71 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.js
@@ -1,29 +1,29 @@
 function solution(s) {
-  // 문자열의 첫글자와 같은 글자, 다른 글자를 각각 카운트하는 변수 -> 투 포인터
+  // 문자열의 첫글자와 같은 글자, 다른 글자를 각각 카운트하는 변수
   // 두 변수의 값이 같아지는 부분만큼의 문자열을 반환
   // 문자열 s의 길이만큼 반복 -> 읽을 글자가 없거나 문자열이 끝나면 종료
-let answer = 0;
+  let answer = [];
 
-let start = 0;
-let same = 0;
-let diff = 0;
+  function helper(str) {
+    let same = 0;
+    let diff = 0;
 
-for (let i = 0; i < s.length; i++) {
-  if (s[start] === s[i]) {
-    same++;
-  } else {
-    diff++;
+    for (let i = 0; i < str.length; i++) {
+      if (str[0] === str[i]) {
+        same++;
+      } else {
+        diff++;
+      }
+      if (same === diff) {
+        answer.push(str.slice(0, i + 1));
+        return helper(str.slice(i + 1));
+      } else if (same !== diff && i === str.length - 1) {
+        return answer.push(str.slice(0));
+      }
+    }
   }
 
-  if (same === diff) {
-    answer++;
-    start = i + 1;
-    same = 0;
-    diff = 0;
-  } else if (same !== diff && i === s.length -1) {
-    answer++;
-  }
-}
+  helper(s);
 
-return answer;
+  return answer.length;
 }
