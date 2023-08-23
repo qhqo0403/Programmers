87968c51d04a3361a42fd79cda3442c2c237eb28
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-23 23:33:27 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,13 +1,3 @@
 function solution(myString) {
-    let answer = '';
-    
-    for (let letter of myString) {
-        let charCode = letter.charCodeAt();
-        if (charCode >= 97 && charCode <= 122) {
-            letter = letter.toUpperCase();
-        }
-        answer += letter;
-    }
-    
-    return answer;
+    return myString.toUpperCase();
 }
