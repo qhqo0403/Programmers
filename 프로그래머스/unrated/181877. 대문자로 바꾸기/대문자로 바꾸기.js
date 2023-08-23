9ab91d7a7e4391f0bc0415186cc1487dec75a25a
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-23 23:34:08 +0900

    Update 대문자로 바꾸기.js

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,3 +1,18 @@
 function solution(myString) {
     return myString.toUpperCase();
 }
+
+function solution(myString) {
+    let answer = '';
+    
+    for (let letter of myString) {
+        let charCode = letter.charCodeAt();
+        if (charCode >= 97 && charCode <= 122) {
+            letter = letter.toUpperCase();
+        }
+        answer += letter;
+    }
+    
+    return answer;
+}
+// 간단하게 생각하기!
