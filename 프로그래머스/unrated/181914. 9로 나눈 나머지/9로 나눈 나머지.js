1bc2b32d4d3878c1c88851633d53f42a9ca58330
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-25 23:25:56 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -0,0 +1,11 @@
+function solution(number) {
+    // number는 음이 아닌 정수
+    // 각 자릿수를 다 더한 후 9로 나눈 나머지 구하기
+    let sum = 0;
+    
+    for (let n of number) {
+        sum += n*1;
+    }
+    
+    return sum % 9;
+}
