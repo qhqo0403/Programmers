396f2ae07e5918beb34cb0a5e0ac8ac029c8a1cb
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-25 22:49:31 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 3.09 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,3 +1,19 @@
 function solution(myString, pat) {
-    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
+    let answer = 0;
+    const str = myString.toLowerCase();
+    const pattern = pat.toLowerCase();
+    
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < pattern.length; j++) {
+            if (pattern[j] !== str[i + j]) break;
+            if (j === pattern.length - 1) answer = 1;
+        }
+    }
+    return answer;
 }
+
+
+// 메서드 이용
+//function solution(myString, pat) {
+//    return myString.toLowerCase().includes(pat.toLowerCase()) ? 1 : 0;
+//}
