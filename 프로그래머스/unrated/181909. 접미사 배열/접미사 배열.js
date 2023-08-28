48998317aacb961e69414706523b5028900d2cd0
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-08-28 21:01:21 +0900

    Update 접미사 배열.js

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.js
@@ -1,6 +1,17 @@
 function solution(my_string) {
   const answer = [];
   
+  for (let i = 0; i < my_string.length; i++) {
+      answer.push(my_string.slice(i));
+  }
+  
+  return answer.sort();
+}
+
+// 재귀함수로
+function solution(my_string) {
+  const answer = [];
+  
   function helper(str) {
     if (str.length === 0) {
       return;
