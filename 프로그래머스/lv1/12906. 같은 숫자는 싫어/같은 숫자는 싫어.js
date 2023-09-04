9fcca5222b08e12b54128d8a31a35671e8af2f8f
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-05 02:21:51 +0900

    Update 같은 숫자는 싫어.js

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,17 @@
 function solution(arr) {
+  const result = [arr[0]];
+  let i = 0;
+  for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+          result.push(arr[j]);
+          i = j;
+      } 
+  }
+  return result;
+}
+
+// 다른 풀이
+function another(arr) {
     return arr.filter((n, idx) => n !== arr[idx+1]);
 }
+// 문제에 있던 효율성은 비슷했지만 다른 테스트케이스를 통과하는데에는 밑의 방식이 훨씬 빨랐음
