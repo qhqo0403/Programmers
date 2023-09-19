51fe5f191f7fd3b18700ff0a785004aba7ce884f
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-19 21:13:44 +0900

    Update 두 개 뽑아서 더하기.js

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -12,3 +12,19 @@ function solution(numbers) {
     const answer = Object.keys(numCount);
     return answer.map(Number);
 }
+
+// 다른 풀이
+function another(numbers) {
+    const temp = []
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            temp.push(numbers[i] + numbers[j])
+        }
+    }
+
+    const answer = [...new Set(temp)]
+
+    return answer.sort((a, b) => a - b)
+}
+// new Set() 으로 중복값을 제거할 수 있음
