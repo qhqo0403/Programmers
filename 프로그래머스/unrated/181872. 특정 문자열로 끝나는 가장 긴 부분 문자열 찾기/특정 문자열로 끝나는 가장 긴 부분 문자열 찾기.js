22c9e111a3d22a32af5626fe472aa6ff3b9e4001
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-09-25 21:17:14 +0900

    Update 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -1,3 +1,17 @@
 function solution(myString, pat) {
+  let answer = '';
+  
+  for (let i = 0; i < myString.length; i++) {
+      let temp = myString.slice(i, pat.length + i);
+      
+      if (temp === pat) {
+          answer = myString.slice(0, pat.length + i);
+      }
+  }
+  return answer;
+}
+
+// lastIndexOf 메서드 이용
+function solution(myString, pat) {
     return myString.slice(0, myString.lastIndexOf(pat)) + pat;
 }
