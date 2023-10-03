0959484a77e03418a1e57f34267fcf18e0fce529
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-10-03 20:56:09 +0900

    [unrated] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,13 @@
+function solution(n, slicer, num_list) {
+    let [a, b, c] = slicer;
+    
+    if (n === 1) {
+        return num_list.slice(0, b + 1);
+    } else if (n === 2) {
+        return num_list.slice(a);
+    } else if (n === 3) {
+        return num_list.slice(a, b + 1);
+    } else {
+        return num_list.slice(a, b + 1).filter((num, idx) => idx % c === 0);
+    }
+}
