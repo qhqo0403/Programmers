2dea70fb1c7477c1010554034ce29c77e61b58dd
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-11-28 21:47:36 +0900

    Update 카드 뭉치.js

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -12,3 +12,20 @@ function solution(cards1, cards2, goal) {
     }
     return "Yes";
 }
+// 다른풀이
+function solution(cards1, cards2, goal) {
+
+    for(const s of goal) {
+
+        if(cards1[0] == s) {
+            cards1.shift();
+        } else if(cards2[0] == s) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
+    }
+
+    return "Yes";
+}
+// 확인하고 맞으면 하나씩 앞에서부터 제거
