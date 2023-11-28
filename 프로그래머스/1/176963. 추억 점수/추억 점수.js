c215f6b973db9ab452a41359e6861260d1cabf92
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-11-28 20:43:23 +0900

    [level 1] Title: 추억 점수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,25 @@
+function solution(name, yearning, photo) {
+    // 이름과 추억점수를 객체로 저장해서 키(이름)값으로 접근
+    // 찾는 키값이 있으면 점수를 더하고 아니면 넘어가기 -> 점수가 더해질 변수 필요
+    const value = {};
+    const result = [];
+    let point = 0;
+    
+    for (let i = 0; i < name.length; i++) {
+        value[name[i]] = yearning[i];
+    };
+    
+    photo.forEach(each => {
+        for (let j = 0; j < each.length; j++) {
+            if (value[each[j]]) {
+                point += value[each[j]]
+            };
+            if (j === each.length - 1) {
+                result.push(point);
+                point = 0;
+            }
+        }
+    });
+    
+    return result;
+}
