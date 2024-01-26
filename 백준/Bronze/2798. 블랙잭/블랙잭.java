d1bc9e2bfaa1802c8cd487b316e32f44d9a7670e
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2024-01-26 23:40:38 +0900

    Update 블랙잭.java

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -13,8 +13,8 @@ public class Main {
 		}
 		
 		int cardSum = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = i + 1; j < N; j++) {
+		for (int i = 0; i <= N - 2; i++) {
+			for (int j = i + 1; j <= N - 1; j++) {
 				for (int k = j + 1; k < N; k++) {
 					int tempMax = cards[i] + cards[j] + cards[k];
 					if (tempMax <= maxSum) {
