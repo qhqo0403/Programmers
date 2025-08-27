7a6e88a550f2044a5793b817363074c4bbf31959
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2025-08-27 21:05:52 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정한 문자를 대문자로 바꾸기 - 181873 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181873) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.28 ms
+메모리: 33.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 27일 21:00:01
+2025년 08월 27일 21:05:52
 
 ### 문제 설명
 
--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -1,14 +1,21 @@
 function solution(my_string, alp) {
-    let answer = "";
+//     let answer = "";
     
-    for (let i = 0; i < my_string.length; i++) {
-        if (my_string[i] === alp) {
-            answer += my_string[i].toUpperCase();
-        } else {
-            answer += my_string[i]
-        }
+//     for (let i = 0; i < my_string.length; i++) {
+//         if (my_string[i] === alp) {
+//             answer += my_string[i].toUpperCase();
+//         } else {
+//             answer += my_string[i]
+//         }
+//     }
+    
+//     return answer;
+    
+    if (my_string.includes(alp)) {
+        return my_string.replaceAll(alp, alp.toUpperCase());
+    } else {
+        return my_string;
     }
     
-    return answer;
     
 }
